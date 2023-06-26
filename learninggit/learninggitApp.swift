7ddccbadf4b901bca5cfe01daa0460434a77ff6e
Joyce Xu <joycejzxu@gmail.com>
2023-06-26 15:28:30 -0400

    Initial Commit

---
+++ learninggit/learninggitApp.swift
@@ -0,0 +1,17 @@
+//
+//  learninggitApp.swift
+//  learninggit
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct learninggitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
